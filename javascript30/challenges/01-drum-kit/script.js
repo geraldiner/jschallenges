6ba4bdd071989c1b9213df37911c602516948de8
geraldiner <hello@geraldiner.com>
2021-01-09 19:58:34 -1000

    fix styling

--- javascript30/challenges/01-drum-kit/script.js
+++ javascript30/challenges/01-drum-kit/script.js
@@ -11,29 +11,27 @@ window.addEventListener("keydown", function (e) {
 });
 
 function removeTransition(e) {
-  //if (e.propertyName !== "background-color") return;
-  console.log("HELLO")
+  if (e.propertyName !== "background-color") return;
   e.target.classList.remove("playing");
   this.classList.remove("playing");
 }
 
 const keys = document.querySelectorAll("div.key");
-console.log(keys)
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
-// // For sticky nav when scrolling
-// const nav = document.querySelector("#stick-nav");
-// const topOfNav = nav.offsetTop;
-// const wrap = document.querySelector(".wrap");
+// For sticky nav when scrolling
+const nav = document.querySelector("#nav");
+const topOfNav = nav.offsetTop;
+const wrap = document.querySelector(".wrap");
 
-// function fixNav() {
-//   if (window.scrollY >= topOfNav) {
-//     wrap.style.paddingTop = nav.offsetHeight;
-//     wrap.classList.add("fixed-nav");
-//   } else {
-//     wrap.style.paddingTop = 0;
-//     wrap.classList.remove("fixed-nav");
-//   }
-// }
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    wrap.style.paddingTop = nav.offsetHeight;
+    wrap.classList.add("fixed-nav");
+  } else {
+    wrap.style.paddingTop = 0;
+    wrap.classList.remove("fixed-nav");
+  }
+}
 
-// window.addEventListener("scroll", fixNav);
+window.addEventListener("scroll", fixNav);
