2c08e438e0b40d0dc3a9e3a3317a328ffdd4bdc5
geraldiner <hello@geraldiner.com>
2021-03-26 12:44:31 -1000

    add day 1

---
+++ javascript30/challenges/01-drum-kit/drum-kit.js
@@ -0,0 +1,38 @@
+// JavaScript30 JS
+
+window.addEventListener("keydown", function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  var div = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  div.classList.add("playing");
+});
+
+function removeTransition(e) {
+  console.log(e);
+  if (e.propertyName !== "background-color") return;
+  e.target.classList.remove("playing");
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+// For sticky nav when scrolling
+const nav = document.querySelector(".nav");
+const topOfNav = nav.offsetTop;
+const wrap = document.querySelector(".wrap");
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    wrap.style.paddingTop = nav.offsetHeight;
+    wrap.classList.add("fixed-nav");
+  } else {
+    wrap.style.paddingTop = 0;
+    wrap.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
